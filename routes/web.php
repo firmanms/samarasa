3520fad0c2ac633b827f7bc5ab494e3f9f86bd50
DESKTOP-NHNBUA2\Firman <firmanmuhamadsahidin@gmail.com>
2024-09-25 15:10:07 +0700

    setting livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+//setting apabila notfound
+ Livewire::setScriptRoute(function ($handle) {
+ return Route::get('/kerudung/livewire/livewire.js', $handle);
+   });
+
+   Livewire::setUpdateRoute(function ($handle) {
+   return Route::post('/kerudung/livewire/update', $handle);
+   });
 
 Route::get('/', function () {
     return view('frontend.index');
