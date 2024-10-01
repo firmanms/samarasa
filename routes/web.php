0f3082068d6fb586b9e20b9082be3a43d2d7604c
DESKTOP-NHNBUA2\Firman <firmanmuhamadsahidin@gmail.com>
2024-10-01 23:13:12 +0700

    editroute

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 
-Route::get('/', [FrontendController::class, 'index']);
-Route::post('/', [FrontendController::class, 'search'])->name('sekolah.cari');
+Route::get('/samarasa', [FrontendController::class, 'index']);
+Route::post('/samarasa', [FrontendController::class, 'search'])->name('sekolah.cari');
 
  Livewire::setScriptRoute(function ($handle) {
  return Route::get('/samarasa/livewire/livewire.js', $handle);
