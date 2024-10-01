bc5a4aa38a36cc94d11bc1fae3df98ff166a1668
DESKTOP-NHNBUA2\Firman <firmanmuhamadsahidin@gmail.com>
2024-10-01 15:43:33 +0700

    routeon

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 Route::get('/', [FrontendController::class, 'index']);
 Route::post('/', [FrontendController::class, 'search'])->name('sekolah.cari');
+
+ Livewire::setScriptRoute(function ($handle) {
+ return Route::get('/samarasa/livewire/livewire.js', $handle);
+   });
+
+   Livewire::setUpdateRoute(function ($handle) {
+   return Route::post('/samarasa/livewire/update', $handle);
+   });
